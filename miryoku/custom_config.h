fcee9925fef1f00b3cb2f055bcf2f22cd5d680cc
Jordan-PC <jordan.kozlowski@gmail.com>
2026-01-20 15:31:35 -0500

    Revert to base and start again

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,7 @@
 
 #define MIRYOKU_LAYER_BASE                                                                                                                           \
 	&kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P,                                                                            \
-		U_MT(LGUI, A), U_MT(LALT, S), U_MT(LCTRL, D), U_MT(LSHFT, F), &kp G, &kp H, U_MT(LSHFT, J), U_MT(LCTRL, K), U_MT(LALT, L), U_MT(LGUI, SEMI), \
+		U_HML(LGUI, A), U_HML(LALT, S), U_HML(LCTRL, D), U_HML(LSHFT, F), &kp G, &kp H, U_HMR(LSHFT, J), U_HMR(LCTRL, K), U_HMR(LALT, L), U_HMR(LGUI, SEMI), \
 		U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C, &kp V, &kp B, &kp N, &kp M, &kp COMMA, U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH),                      \
 		U_NP, U_NP, U_LT(U_MOUSE, TAB), U_LT(U_NAV, SPACE), U_LT(U_MEDIA, ESC), U_LT(U_FUN, DEL), U_LT(U_NUM, RET), U_LT(U_SYM, BSPC), U_NP, U_NP
 
