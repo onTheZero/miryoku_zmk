5bdb02653b503c5765fa7e5544811e9ad19dcdee
Jordan-PC <jordan.kozlowski@gmail.com>
2026-01-20 19:35:09 -0500

    I had mixed up HMR and HML trigger positions

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -24,7 +24,7 @@
 #define U_NU &none // available but not used
 
 #define U_TAPPING_TERM 280
-#define U_IDLE_TERM 50
+#define U_IDLE_TERM 150
 #define U_QUICK_TERM 175
 
 #include "miryoku_clipboard.h"
